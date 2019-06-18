729ebcbd0a9634e2bc632255fef52ddd89ad6cb5
Richard Moore <rmooreau@outlook.com>
2019-06-18 10:54:18 +1000

    Updated to 1.1

--- discordbot/server/main.js
+++ discordbot/server/main.js
@@ -1,3 +1,33 @@
+/**
+	Discord bot by Circleus
+	Updated by Ozzy and MERZBAU (most of code)
+	version 1.1
+	Update is to address issue with Rcon timing out due to the way SRCDS handles timeouts on RCON
+
+GIT Repos:
+Circleus: https://github.com/zWolfi/INS_sourcemod
+Ozzy: https://github.com/ozzygit/Insurgency_Sourcemod
+
+Discord:
+Circleus [TEC] - Terror Error Community: https://discord.gg/G2SMMnU 
+Ozzy: [SOC] - https://discord.gg/3BbGmZR
+
+This discord bot enables you to send messages to your game from discord
+The sourcemod plugin discordchat.sp is required and discord.sp is also recommended.
+
+You'll need to install nodejs and npm to run. I (ozzy) also recommend you use forever so it remains running when terminal is closed (if linux)
+This will need to run as long as the server is running. If this is shutdown, the bot will go offline
+
+Look at the commented lines below and fill out your server IP, port, rcon_password, and your bot token.
+
+Read through everything and edit it.
+After all that, all you have to do is to run the main.js from the server folder and you will have to invite your bot to your discord server.
+Bot will show up in your discord server and you will have to give the permission to the bot strictly. Otherwise the bot will send all the messages from all the text channel to in game chat.
+
+FULL credit goes to Circleus for the development of the plugin and his support to Ozzy. Credit goes to MERZBAU with getting the RCON timeout issue fixed.
+*/
+
+
 //Discord
 var Discord = require("discord.js");
 var bot = new Discord.Client();
